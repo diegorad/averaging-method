223cb7a7f6d96e49a9a333c7a006179dea1b23e9
diegorad <87992417+diegorad@users.noreply.github.com>
2022-10-31 15:15:44 -0600

    Add files via upload

---
+++ blender_mesh_export.py
@@ -0,0 +1,25 @@
+import bpy
+
+me = bpy.context.object.data
+
+filepath = "/Users/diegorad/Documents/Averaging/test_mesh.dat"
+f = open(filepath, "w")
+
+f.write("%d %d %d\n" % (len(me.vertices),len(me.edges),len(me.polygons)))
+
+verts = [ bpy.context.object.matrix_world @ v.co for v in me.vertices ]
+ssvLines = [ " ".join([str(v) for v in co]) + "\n" for co in verts]
+f.writelines(ssvLines)
+
+for poly in me.polygons:
+    f.write("%d\n" % poly.loop_total)
+
+for poly in me.polygons:    
+    for loop_index in range(poly.loop_start, poly.loop_start + poly.loop_total):
+        f.write("%d " % me.loops[loop_index].vertex_index)
+    f.write("\n")
+    
+for polygon in me.polygons:
+    f.write("%lf %lf\n" % (polygon.center[0],polygon.center[1]))
+
+f.close()         
